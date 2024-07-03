f43d401f386d2f2414659abc1487bbc8a7eedacf
VikashAnandJha <mailtovikashjha@gmail.com>
2024-07-04 04:22:30 +0530

    signin page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,17 +1,53 @@
-"use client";
-import Image from "next/image";
-import "@fontsource/roboto/300.css";
-import ChatListComponent from "@/components/ChatUI/ChatListComponent";
-import { UserProvider } from "./UserContext";
-import { Button } from "@mui/material";
+import React from "react";
+import { Box, Button, Typography, Container } from "@mui/material";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
+const LandingPage = () => {
   return (
-    <main className="flex flex-col min-h-screen  items-center ">
-      <Link href={"/chat"}>
-        <Button>Enter Chat</Button>
-      </Link>
-    </main>
+    <Container component="main" maxWidth="md">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 8,
+        }}
+      >
+        {" "}
+        <Link href={"/"}>
+          <Image
+            src="/assets/logo_biradar.png"
+            alt="Logo"
+            width={200}
+            height={50}
+          />
+        </Link>
+        <Typography component="h1" variant="h2" gutterBottom>
+          Welcome to Biradar
+        </Typography>
+        <Typography component="p" variant="h5" align="center" gutterBottom>
+          Biradar is a chat/social/teams app designed to keep you connected with
+          your friends, family, and colleagues.
+        </Typography>
+        <Typography component="p" variant="h5" align="center" gutterBottom>
+          Stay in touch, collaborate, and share moments all in one place.
+        </Typography>
+        <Box sx={{ mt: 4, display: "flex", gap: 2 }}>
+          <Link href="/signup" passHref>
+            <Button variant="contained" color="primary">
+              Sign Up
+            </Button>
+          </Link>
+          <Link href="/signin" passHref>
+            <Button variant="outlined" color="primary">
+              Sign In
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
+
+export default LandingPage;
---
+++ frontend/src/app/signin/page.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  TextField,
+  Button,
+  Box,
+  Typography,
+  Container,
+  Avatar,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Image from "next/image";
+import Link from "next/link";
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 8,
+        }}
+      >
+        <Link href={"/"}>
+          <Image
+            src="/assets/logo_biradar.png"
+            alt="Logo"
+            width={300}
+            height={100}
+          />
+        </Link>
+
+        <section>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </section>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
