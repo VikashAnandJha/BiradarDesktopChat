284925d591a81a46450ef98860dff0f4d1736734
VikashAnandJha <mailtovikashjha@gmail.com>
2024-07-14 00:03:33 +0530

    update with message

--- frontend/src/app/chat/page.js
+++ frontend/src/app/chat/page.js
@@ -3,16 +3,19 @@ import ChatListComponent from "@/components/ChatUI/ChatListComponent";
 import SearchComponent from "@/components/ChatUI/SearchComponent";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Box, IconButton } from "@mui/material";
 import { LogoutOutlined, SearchOutlined } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 import { UserProvider } from "../UserContext";
 import axiosInstance from "@/utils/axiosConfig";
+import { io } from "socket.io-client";
 
 function AllChatScreen({ params }) {
+  const socketRef = useRef();
   const [convList, setConvList] = useState([]);
   const router = useRouter();
+
   const handleLogout = async () => {
     localStorage.removeItem("token");
     router.replace("/");
@@ -27,7 +30,7 @@ function AllChatScreen({ params }) {
       try {
         const response = await axiosInstance.get(`/chat/getAllConv`, {});
         const data = response.data;
-        console.log(data);
+        console.log("Fetched conversations:", data);
         if (data.success) {
           setConvList(data.conversations);
         } else {
@@ -38,10 +41,45 @@ function AllChatScreen({ params }) {
       }
     };
 
-    if (true) {
-      fetchAllConv();
-    }
+    fetchAllConv();
   }, []);
+
+  useEffect(() => {
+    function alreadyInList(userId) {
+      return (
+        convList.filter((conv) => conv.target_user._id === userId).length > 0
+      );
+    }
+    function updateMessageText(userId, text) {
+      let updatedConvList = convList.map((conv) => {
+        if (conv.target_user._id === userId) {
+          return {
+            ...conv,
+            last_message: text,
+          };
+        }
+        return conv;
+      });
+
+      setConvList(updatedConvList);
+    }
+
+    socketRef.current = io("http://localhost:3009");
+
+    socketRef.current.on("receive_message", (message) => {
+      console.log("Received message: ", message);
+
+      if (alreadyInList(message.from_user)) {
+        //just update the msg
+        updateMessageText(message.from_user, message.message);
+      }
+    });
+
+    return () => {
+      console.log("Disconnecting from socket...");
+      socketRef.current.disconnect();
+    };
+  }, [convList]); // Add convList as a dependency
 
   return (
     <UserProvider>
@@ -66,7 +104,7 @@ function AllChatScreen({ params }) {
             <Link href={"/chat/search"}>
               {" "}
               <IconButton>
-                <SearchOutlined></SearchOutlined>
+                <SearchOutlined />
               </IconButton>
             </Link>
             <Button
